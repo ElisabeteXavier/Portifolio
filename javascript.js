5b6ae9c491d1eec4ac9c81847d61c0c918ead691
ElisabeteXavier <elisabetebx@gmail.com>
2022-12-08 16:58:34 -0300

    scroll

--- javascript.js
+++ javascript.js
@@ -36,3 +36,21 @@ function botaoMenu(){
     }else
     document.getElementById("ul-menu").style.visibility = "visible"
 }
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 300;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
